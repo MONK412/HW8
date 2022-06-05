3806f6c7e70ae7a7392a0635c729e9588922b9ec
Rustam Mustafin <rustammuus@mail.ru>
2022-06-05 20:14:11 +0300

    HW10

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -7,40 +7,72 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class RadioTest {
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataMinMaxNumber.csv"})
-    void findMinMaxNumber(int number, int expected) {
+    @CsvFileSource(resources = {"/minmaxnumber.csv"})
+    void findMinMaxCurrentNumberDefault(int currentNumber, int expected) {
         Radio rad = new Radio();
-        rad.setNumber(number);
-        int actual = rad.getNumber();
+        rad.setCurrentNumber(currentNumber);
+        int actual = rad.getCurrentNumber();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataNextNumber.csv"})
-    void nextNumber(int number, int expected) {
-        Radio rad = new Radio();
-        rad.setNumber(number);
-        rad.nextNumber();
-        int actual = rad.getNumber();
+    @CsvFileSource(resources = {"/minmaxnumber1.csv"})
+    void findMinMaxCurrentNumberNotDefault(int currentNumber, int expected) {
+        Radio rad = new Radio(30);
+        rad.setCurrentNumber(currentNumber);
+        int actual = rad.getCurrentNumber();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataPrevNumber.csv"})
-    void prevNumber(int number, int expected) {
+    @CsvFileSource(resources = {"/nextnumber.csv"})
+    void nextCurrentNumberDefault(int currentNumber, int expected) {
         Radio rad = new Radio();
-        rad.setNumber(number);
-        rad.prevNumber();
-        int actual = rad.getNumber();
+        rad.setCurrentNumber(currentNumber);
+        rad.nextCurrentNumber();
+        int actual = rad.getCurrentNumber();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataMinMaxVolume.csv"})
-    void findMinMaxVolume(int currentVolume, int expected) {
+    @CsvFileSource(resources = {"/nextnumber1.csv"})
+    void nextCurrentNumberNotDefault(int currentNumber, int expected) {
+        Radio rad = new Radio(30);
+        rad.setCurrentNumber(currentNumber);
+        rad.nextCurrentNumber();
+        int actual = rad.getCurrentNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/prevnumber.csv"})
+    void prevCurrentNumberDefault(int currentNumber, int expected) {
+        Radio rad = new Radio();
+        rad.setCurrentNumber(currentNumber);
+        rad.prevCurrentNumber();
+        int actual = rad.getCurrentNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/prevnumber1.csv"})
+    void prevCurrentNumberNotDefault(int currentNumber, int expected) {
+        Radio rad = new Radio(30);
+        rad.setCurrentNumber(currentNumber);
+        rad.prevCurrentNumber();
+        int actual = rad.getCurrentNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/minmaxvolume.csv"})
+    void findMinMaxVolumeDefault(int currentVolume, int expected) {
         Radio rad = new Radio();
         rad.setCurrentVolume(currentVolume);
         int actual = rad.getCurrentVolume();
@@ -49,8 +81,18 @@ public class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataIncreaseVolume.csv"})
-    void increseVolume(int currentVolume, int expected) {
+    @CsvFileSource(resources = {"/minmaxvolume.csv"})
+    void findMinMaxVolumeNotDefault(int currentVolume, int expected) {
+        Radio rad = new Radio(30);
+        rad.setCurrentVolume(currentVolume);
+        int actual = rad.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/increasevolume.csv"})
+    void increseVolumeDefault(int currentVolume, int expected) {
         Radio rad = new Radio();
         rad.setCurrentVolume(currentVolume);
         rad.increaseVolume();
@@ -60,9 +102,31 @@ public class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = {"/testdataTurnDownVolume.csv"})
-    void turnDownVolume(int currentVolume, int expected) {
+    @CsvFileSource(resources = {"/increasevolume.csv"})
+    void increseVolumeNotDefault(int currentVolume, int expected) {
+        Radio rad = new Radio(30);
+        rad.setCurrentVolume(currentVolume);
+        rad.increaseVolume();
+        int actual = rad.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/turndownvolume.csv"})
+    void turnDownVolumeDefoult(int currentVolume, int expected) {
         Radio rad = new Radio();
+        rad.setCurrentVolume(currentVolume);
+        rad.turnDownVolume();
+        int actual = rad.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/turndownvolume.csv"})
+    void turnDownVolumeNotDefoult(int currentVolume, int expected) {
+        Radio rad = new Radio(30);
         rad.setCurrentVolume(currentVolume);
         rad.turnDownVolume();
         int actual = rad.getCurrentVolume();
